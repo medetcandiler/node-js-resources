8bf482a5e44e88a7de119871fc5283a838b14b06
Medetcan Diler <medetdiler1996@gmail.com>
2024-01-06 00:10:42 +0300

    Recapped jwt operations

--- 11-JWT/server/controllers/registerController.js
+++ 11-JWT/server/controllers/registerController.js
@@ -10,8 +10,8 @@ const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     // check for duplicate usernames in the db
-    const duplicate = usersDB.users.find(person => person.username === user);
-    if (duplicate) return res.sendStatus(409); //Conflict 
+    const isDuplicate = usersDB.users.find(person => person.username === user);
+    if (isDuplicate) return res.sendStatus(409); //Conflict 
     try {
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
